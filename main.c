2c5797e124e45f01defc9a68a847a22139144277
DanielD <dobinski.daniel@gmail.com>
2022-10-26 20:07:14 +0200

    Pieces added
    Pieces are added as a construct for further development
    They are not visible on the board yet

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include "piece.h"
 #include "main.h"
 
+struct Field
+{
+	bool isEmpty;
+	enum Piece piece;
+	enum PieceColor color;
+};
+
+struct Board
+{
+	struct Field fields[BoardWidth][BoardHeigth];
+};
+
+static struct Board board;
 
 void showColumnNames (void)
 {
@@ -16,29 +33,47 @@ void showRowSeparator (void)
 	putchar(' ');
 	for (int i = 0; i < BoardWidth; ++i)
 	printf("+-");
+
     putchar('+');
 	putchar('\n');
 }
 
-void showRow (int row)
+void showRow (struct Board * board, int row)
 {
 	printf("%u", row + 1);
-	for (int i = 0; i < BoardWidth; ++i)
-	printf("| ");
+	for (int col = 0; col < BoardWidth; ++col)
+	{
+		struct Field *currentField = &(board->fields[row][col]);
+		if(currentField->isEmpty == true)
+			printf("| ");
+		else
+			printf("|B");
+	}
 	printf("|");
 	printf("%u", row + 1);
 	putchar('\n');
 }
-	
+
+struct Board* initBoard()
+{
+	memset(&board, 0, BoardHeigth * BoardWidth * sizeof(struct Field));
+	return &board;
+}
+
 int main()
 {
+	initBoard();
+	struct Field *field1 = &(board.fields[0][0]);
+	field1->isEmpty = true;
+
+
     printf("Board \n\n");
 	showColumnNames();
 	int row = BoardHeigth;
 	while(row--)
 	{
 		showRowSeparator();
-		showRow(row);
+		showRow(&board, row);
 	}
 	showRowSeparator();
 	showColumnNames();
---
+++ piece.h
@@ -0,0 +1,16 @@
+
+
+enum Piece
+{
+	pawn = 1,
+	knight,
+	bishop,
+	rook,
+	queen,
+};
+
+enum PieceColor
+{
+	white = 0,
+	black = 8
+};
